855ff1701ba09f1265b551122aac026909cd08c3
Rajvardhan <rajthakur3619@gmail.com>
2020-05-05 20:48:18 +0530

    using fetch to dend dat

--- static/js/cart.js
+++ static/js/cart.js
@@ -6,5 +6,35 @@ for(var i=0; i<updateButtons.length; i++){
         var action    = this.dataset.action
 
         console.log('productId : ', productId, 'action : ',action)
+
+        console.log('USER : ', user)
+
+        if(user == 'AnonymousUser'){
+            console.log('User is not logged in')
+        }
+        else{
+            updateUserOrder(productId, action)
+        }
+    })
+
+}
+
+function updateUserOrder(productId, action){
+    console.log('User is logged in sending data...')
+
+    var url = '/update_item/'
+
+    fetch(url, {
+        method: 'POST',
+        headers : {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((data)=>{
+        console.log('data ', data)
     })
 }
