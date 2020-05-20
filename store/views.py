22883bfe8ba6a271ad40929c845ff331587ee389
Rajvardhan <rajthakur3619@gmail.com>
2020-05-21 01:22:50 +0530

    Adding the shippping info is the user is logged in or not if login we don't need user's name or email

--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
-import json 
+import json
+import datetime 
 
 # Create your views here.
 
@@ -76,3 +77,34 @@ def updateItem(request):
         orderItem.delete()
 
     return JsonResponse('item was added' , safe=False)
+
+
+def processOrder(request):
+    
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, customer = Order.objects.get_or_create(customer= customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+
+        if total == order.get_cart_total:
+            order.complete = True
+        
+        order.save()
+
+        if order.shipping == True:
+            ShippingAddress.objects.create(
+                customer=customer,
+                order=order,
+                address= data['shipping']['address'],
+                city= data['shipping']['city'],
+                state=data['shipping']['state'],
+                zipcode=data['shipping']['zipcode'],
+            )
+        
+        else:
+            print("user is not logged in....")
+    return JsonResponse("Payment completed...", safe=False)
